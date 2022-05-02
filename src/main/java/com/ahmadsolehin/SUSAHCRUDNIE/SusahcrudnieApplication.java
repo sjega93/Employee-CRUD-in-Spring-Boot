c8ae154dce842738e732e3b38a6f037f1e04acf2
Your Name <sathishjegatheeswaran@gmx.de>
2022-05-02 17:24:29 +0200

    adding missing imports

--- src/main/java/com/ahmadsolehin/SUSAHCRUDNIE/SusahcrudnieApplication.java
+++ src/main/java/com/ahmadsolehin/SUSAHCRUDNIE/SusahcrudnieApplication.java
@@ -2,6 +2,7 @@ package com.ahmadsolehin.SUSAHCRUDNIE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SusahcrudnieApplication {
