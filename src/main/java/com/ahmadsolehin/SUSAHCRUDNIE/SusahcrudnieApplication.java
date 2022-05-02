21d5d6ef9b0a5070aa375037359cdfc6c1ae6c67
sjega93 <60350416+sjega93@users.noreply.github.com>
2022-05-02 17:00:27 +0200

    Update SusahcrudnieApplication.java

--- src/main/java/com/ahmadsolehin/SUSAHCRUDNIE/SusahcrudnieApplication.java
+++ src/main/java/com/ahmadsolehin/SUSAHCRUDNIE/SusahcrudnieApplication.java
@@ -3,7 +3,7 @@ package com.ahmadsolehin.SUSAHCRUDNIE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SusahcrudnieApplication {
 
 	public static void main(String[] args) {
